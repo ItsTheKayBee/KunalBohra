630fa16c480663d51f07468bbd7cf0938ad95b82
Kunal <kunal.bohra@somaiya.edu>
2020-06-18 01:06:58 +0530

    Added physics to the world

--- main.js
+++ main.js
@@ -1,27 +1,76 @@
 let scene, camera, renderer, stars, starGeo;
-let mars, marsGeom, rover, light, input;
+let mars, marsGeom, rover, light;
 let whl1, whl2, whl3, whl4, whl5, whl6;
-let movefront=0;
+let movefront = 0;
 let rotationSpeed = 0.05;
 y_axis = new THREE.Vector3(0, 1, 0);
+zero_vector = new THREE.Vector3(0, 0, 0);
 var clock = new THREE.Clock();
+var marsRadius = 1000;
+var actions = {};
+var keysActions = {
+    "KeyW": 'acceleration',
+    "KeyS": 'braking',
+    "KeyA": 'left',
+    "KeyD": 'right'
+};
 
+//physics vars
+var gravityConstant = -3.7;
+var physicsWorld;
+var rigidBodies = [];
+var softBodies = [];
+var margin = 0.05;
+var transformAux1;
+var collisionConfiguration;
+var dispatcher;
+var broadphase;
+var solver;
+var dynamicObjects = [];
+
+//heightfield params
+var terrainWidthExtents = 100;
+var terrainDepthExtents = 100;
+var terrainWidth = 128;
+var terrainDepth = 128;
+var terrainHalfWidth = terrainWidth / 2;
+var terrainHalfDepth = terrainDepth / 2;
+var terrainMaxHeight = 8;
+var terrainMinHeight = - 2;
+
+
+Ammo().then(function (AmmoLib) {
+    Ammo = AmmoLib;
+    init();
+    animate();
+});
+
+
+function initPhysics() {
+    // Physics configuration
+    collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+    dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
+    broadphase = new Ammo.btDbvtBroadphase();
+    solver = new Ammo.btSequentialImpulseConstraintSolver();
+    physicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    physicsWorld.setGravity(new Ammo.btVector3(0, gravityConstant, 0));
+    transformAux1 = new Ammo.btTransform();
+}
 
 function scenes() {
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0x000000, 0.00000025);
+    scene.fog = new THREE.FogExp2(0xffffff, 0.00025);
     var axesHelper = new THREE.AxesHelper(5);
     scene.add(axesHelper);
 }
 
 function cam() {
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(0, 60, 100);
+    camera.position.set(0, 1100, 100);
     camera.lookAt(scene.position);
 }
 
 function lights() {
-
     //ambient light
     hlight = new THREE.AmbientLight(0xffffff, 4);
     scene.add(hlight);
@@ -121,21 +170,26 @@ function loadModels() {
 }
 
 function init() {
+    //config graphics
     rend();
     scenes();
-    // loadModels();
+    cam();
+    lights();
+    orbCont();
+
+    //setting listeners
+    window.addEventListener("resize", onWindowResize, false);
+    window.addEventListener('keydown', keydown);
+    window.addEventListener('keyup', keyup);
+
+    //setting physics
+    initPhysics();
+
+    //adding objects
     loadMars();
     addRover();
-    cam();
-    orbCont();
-    lights();
-    // starAdd();
-    animate();
 }
 
-init();
-
-window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -153,7 +207,12 @@ function distance() {
 
 
 function animate() {
-    renderer.clear();
+    var dt = clock.getDelta();
+    updatePhysics(dt);
+    moveRover();
+    // rotateWheels(1);
+    if (rover)
+        camera.lookAt(rover.position);
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
@@ -184,53 +243,115 @@ function moveCam() {
 }
 
 
-document.addEventListener('keydown', function (ev) {
-    switch (ev.keyCode) {
-        case 37: // left
-            // input.direction = 1;
-            break;
-        case 38: // forward
-            // input.power = true;
-            // movefront = 1;
-            break;
-        case 39: // right
-            // input.direction = -1;
-            break;
-        case 40: // back
-            // input.power = false;
-            break;
+function moveRover() {
+    var speed = vehicle.getCurrentSpeedKmHour();
+
+    breakingForce = 0;
+    engineForce = 0;
+
+    if (actions.acceleration) {
+        if (speed < -1)
+            breakingForce = maxBreakingForce;
+        else engineForce = maxEngineForce;
     }
-});
-document.addEventListener('keyup', function (ev) {
-    switch (ev.keyCode) {
-        case 37: // left
-            // input.direction = null;
-            // moveFront()
-            break;
-        case 38: // forward
-            // input.power = null;
-            break;
-        case 39: // right
-            // input.direction = null;
-            break;
-        case 40: // back
-            // input.power = null;
-            break;
+    if (actions.braking) {
+        if (speed > 1)
+            breakingForce = maxBreakingForce;
+        else engineForce = -maxEngineForce / 2;
     }
-});
+    if (actions.left) {
+        if (vehicleSteering < steeringClamp)
+            vehicleSteering += steeringIncrement;
+    }
+    else {
+        if (actions.right) {
+            if (vehicleSteering > -steeringClamp)
+                vehicleSteering -= steeringIncrement;
+        }
+        else {
+            if (vehicleSteering < -steeringIncrement)
+                vehicleSteering += steeringIncrement;
+            else {
+                if (vehicleSteering > steeringIncrement)
+                    vehicleSteering -= steeringIncrement;
+                else {
+                    vehicleSteering = 0;
+                }
+            }
+        }
+    }
+
+    vehicle.applyEngineForce(engineForce, BACK_LEFT);
+    vehicle.applyEngineForce(engineForce, BACK_RIGHT);
+
+    vehicle.setBrake(breakingForce / 2, FRONT_LEFT);
+    vehicle.setBrake(breakingForce / 2, FRONT_RIGHT);
+    vehicle.setBrake(breakingForce, BACK_LEFT);
+    vehicle.setBrake(breakingForce, BACK_RIGHT);
+
+    vehicle.setSteeringValue(vehicleSteering, FRONT_LEFT);
+    vehicle.setSteeringValue(vehicleSteering, FRONT_RIGHT);
+
+    var tm, p, q, i;
+    var n = vehicle.getNumWheels();
+    for (i = 0; i < n; i++) {
+        vehicle.updateWheelTransform(i, true);
+        tm = vehicle.getWheelTransformWS(i);
+        p = tm.getOrigin();
+        q = tm.getRotation();
+        wheelMeshes[i].position.set(p.x(), p.y(), p.z());
+        wheelMeshes[i].quaternion.set(q.x(), q.y(), q.z(), q.w());
+    }
+
+    tm = vehicle.getChassisWorldTransform();
+    p = tm.getOrigin();
+    q = tm.getRotation();
+    chassisMesh.position.set(p.x(), p.y(), p.z());
+    chassisMesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+
+}
+
+function keyup(e) {
+    if (keysActions[e.code]) {
+        actions[keysActions[e.code]] = false;
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+}
+function keydown(e) {
+    if (keysActions[e.code]) {
+        actions[keysActions[e.code]] = true;
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+}
 
 //loads the plain
 function loadMars() {
-    // marsGeom = new THREE.BoxGeometry(650, 17, 17);
-    marsGeom = new THREE.PlaneGeometry(1000, 1000, 100, 100);
-    buildTerrain();
+    //three
+    marsGeom = new THREE.SphereGeometry(marsRadius, 18, 18);
+    // buildTerrain();
     var marsTexture = new THREE.TextureLoader().load('textures/mars_texture.jpg');
-    var marsMaterial =new THREE.MeshBasicMaterial({ map: marsTexture });
+    var marsMaterial = new THREE.MeshBasicMaterial({ map: marsTexture });
     mars = new THREE.Mesh(marsGeom, marsMaterial);
     mars.receiveShadow = true;
-    // mars.position.y = -400;
-    mars.rotation.x = -Math.PI / 2;
     scene.add(mars);
+
+    //ammo
+    var marsShape = new Ammo.btSphereShape(marsRadius);
+    marsShape.setMargin(margin);
+    var groundMass = 0;
+    var groundLocalInertia = new Ammo.btVector3(0, 0, 0);
+    marsShape.calculateLocalInertia(groundMass, groundLocalInertia);
+    var groundTransform = new Ammo.btTransform();
+    groundTransform.setIdentity();
+    groundTransform.setOrigin(new Ammo.btVector3(0, 0, 0));
+    groundTransform.setRotation(new Ammo.btQuaternion(0, 0, 0, 1));
+    var groundMotionState = new Ammo.btDefaultMotionState(groundTransform);
+    var groundBody = new Ammo.btRigidBody(new Ammo.btRigidBodyConstructionInfo(groundMass, groundMotionState, marsShape, groundLocalInertia));
+    physicsWorld.addRigidBody(groundBody);
 }
 
 //creates noisy terrain
@@ -238,23 +359,21 @@ function buildTerrain() {
     for (var i = 0; i < marsGeom.vertices.length; i++) {
         var vertex = marsGeom.vertices[i];
         value = noise.simplex2(vertex.x / 100, vertex.y / 100);
-        vertex.z = Math.abs(value) * 5;
+        vertex.z += Math.abs(value) * 55;
     }
     marsGeom.computeFaceNormals();
     marsGeom.computeVertexNormals();
 }
 
 //returns a wheel
-function addWheel(x, y, z) {
+function createWheel() {
     var sphGeo = new THREE.CylinderBufferGeometry(2.5, 2.5, 2, 32);
     var sphmat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/metal.jpg') });
     var sph = new THREE.Mesh(sphGeo, sphmat);
-    sph.castShadow = true;
-    sph.position.y -= y;
-    sph.position.z += z;
-    sph.position.x += x;
+    var shape = new Ammo.btCylinderShape(1.25, 1.25, 1.25);
+    shape.setMargin(margin);
+    sph.castShadow = sph.receiveShadow = true;
     sph.add(innerWheel());
-    sph.rotation.x = Math.PI / 2;
     return sph;
 }
 
@@ -263,35 +382,124 @@ function innerWheel() {
     var sphGeo = new THREE.TorusGeometry(0.2, 1, 16, 100);
     var sphmat = new THREE.MeshLambertMaterial({ color: 0x000000 });
     var sph = new THREE.Mesh(sphGeo, sphmat);
+    sph.castShadow = sph.receiveShadow = true;
+    var shape = new Ammo.btCylinderShape(0.2, 0.5, 0.2);
+    shape.setMargin(margin);
+    wheel = createModels(sph, shape, null, null, 0);
     return sph;
+}
+
+
+function createModels(object, shape, pos, quat, mass) {
+    var localInertia = new Ammo.btVector3(0, 0, 0);
+    shape.calculateLocalInertia(mass, localInertia);
+    var transform = new Ammo.btTransform();
+    transform.setIdentity();
+    if (pos == null && quat == null) {
+        pos = { x: 0, y: 0, z: 0 };
+        quat = { x: 0, y: 0, z: 0, w: 1 };
+    }
+    transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+    var motionState = new Ammo.btDefaultMotionState(transform);
+    var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+    var body = new Ammo.btRigidBody(rbInfo);
+    object.userData.physicsBody = body;
+    physicsWorld.addRigidBody(body);
+    if (mass > 0) {
+        dynamicObjects.push(object);
+    }
+    return body;
 }
 
 //add rover to scene
 function addRover() {
-    var boxGeo = new THREE.BoxGeometry(30, 4, 18);
+
+    var friction = 1000;
+    var suspensionStiffness = 20.0;
+    var suspensionDamping = 2.3;
+    var suspensionCompression = 4.4;
+    var suspensionRestLength = 0.6;
+    var rollInfluence = 0.2;
+
+    var steeringIncrement = .04;
+    var steeringClamp = .5;
+    var maxEngineForce = 2000;
+    var maxBreakingForce = 100;
+
+
+    //chassis mesh
+    var boxGeo = new THREE.BoxBufferGeometry(30, 4, 18);
     var boxMat = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/gold.jpg') });
     var chassis = new THREE.Mesh(boxGeo, boxMat);
-    chassis.position.y = 8;
-
-    //create and add wheels
-    whl1 = addWheel(10, 3, 10);
-    chassis.add(whl1);
-    whl2 = addWheel(10, 3, -10);
-    chassis.add(whl2);
-    whl3 = addWheel(0, 3, 10);
-    chassis.add(whl3);
-    whl4 = addWheel(0, 3, -10);
-    chassis.add(whl4);
-    whl5 = addWheel(-10, 3, 10);
-    chassis.add(whl5);
-    whl6 = addWheel(-10, 3, -10);
-    chassis.add(whl6);
+    rover = chassis;
+    var shape = new Ammo.btBoxShape(new Ammo.btVector3(15, 2, 9));
+    shape.setMargin(margin);
+    chassis.castShadow = chassis.receiveShadow = true;
 
     //add solar panel
     chassis.add(addPanel());
 
-    chassis.castShadow = chassis.receiveShadow = true;
-    rover = chassis;
+    //create rigid body
+    var mass = 27000;
+    var pos = { x: 0, y: marsRadius + 2, z: 0 };
+    let quat = { x: 0, y: 0, z: 0, w: 1 };
+    body = createModels(chassis, shape, pos, quat, mass);
+
+    var engineForce = 0;
+    var vehicleSteering = 0;
+    var breakingForce = 0;
+    var tuning = new Ammo.btVehicleTuning();
+    var rayCaster = new Ammo.btDefaultVehicleRaycaster(physicsWorld);
+    var rover = new Ammo.btRaycastVehicle(tuning, body, rayCaster);
+    rover.setCoordinateSystem(0, 1, 2);
+    physicsWorld.addAction(rover);
+
+    var FRONT_LEFT = 0;
+    var FRONT_RIGHT = 1;
+    var MIDDLE_LEFT = 2;
+    var MIDDLE_RIGHT = 3;
+    var BACK_LEFT = 4;
+    var BACK_RIGHT = 5;
+    var wheelMeshes = [];
+    var wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0);
+    var wheelAxleCS = new Ammo.btVector3(-1, 0, 0);
+
+
+    function addWheel(isFront, pos, radius, index) {
+
+        var wheelInfo = rover.addWheel(
+            pos,
+            wheelDirectionCS0,
+            wheelAxleCS,
+            suspensionRestLength,
+            radius,
+            tuning,
+            isFront);
+
+        wheelInfo.set_m_suspensionStiffness(suspensionStiffness);
+        wheelInfo.set_m_wheelsDampingRelaxation(suspensionDamping);
+        wheelInfo.set_m_wheelsDampingCompression(suspensionCompression);
+        wheelInfo.set_m_frictionSlip(friction);
+        wheelInfo.set_m_rollInfluence(rollInfluence);
+
+        wheelMeshes[index] = sph = createWheel();
+        sph.position.x += pos.x;
+        sph.position.y -= pos.y;
+        sph.position.z += pos.z;
+        sph.rotation.x = Math.PI / 2;
+        pos = { x: sph.position.x + pos.x, y: sph.position.y - pos.y, z: sph.position.z + pos.z };
+        quat = { x: 0, y: 0, z: 0, w: 1 };
+        wheel = createModels(sph, shape, pos, quat, 0);
+        chassis.add(wheel);
+    }
+
+    addWheel(true, new Ammo.btVector3(10, 3, 10), 2.5, FRONT_LEFT);
+    addWheel(true, new Ammo.btVector3(-10, 3, -10), 2.5, FRONT_RIGHT);
+    addWheel(false, new Ammo.btVector3(0, 3, 10), 2.5, MIDDLE_LEFT);
+    addWheel(false, new Ammo.btVector3(0, 3, -10), 2.5, MIDDLE_RIGHT);
+    addWheel(false, new Ammo.btVector3(-10, 3, 10), 2.5, BACK_LEFT);
+    addWheel(false, new Ammo.btVector3(-10, 3, -10), 2.5, BACK_RIGHT);
 
     scene.add(chassis);
 }
@@ -303,10 +511,9 @@ function addPanel() {
     var panel = new THREE.Mesh(boxGeo, boxMat);
     panel.rotation.x = Math.PI / 4;
     panel.position.z += 2.5;
-    panel.position.y += 8.5;
+    panel.position.y += 8;
     return panel;
 }
-
 
 //to rotate wheels
 function rotateWheels(dir) {
@@ -316,4 +523,22 @@ function rotateWheels(dir) {
     whl4.rotation.y += dir * 0.2;
     whl5.rotation.y += dir * 0.2;
     whl6.rotation.y += dir * 0.2;
+}
+
+
+function updatePhysics(deltaTime) {
+    physicsWorld.stepSimulation(deltaTime, 10);
+
+    for (let i = 0; i < dynamicObjects.length; i++) {
+        let objThree = dynamicObjects[i];
+        let objAmmo = objThree.userData.physicsBody;
+        let ms = objAmmo.getMotionState();
+        if (ms) {
+            ms.getWorldTransform(transformAux1);
+            var position = transformAux1.getOrigin();
+            var quaternion = transformAux1.getRotation();
+            objThree.position.set(position.x(), position.y(), position.z());
+            objThree.rotation.set(quaternion.x(), quaternion.y(), quaternion.z(), quaternion.w());
+        }
+    }
 }
